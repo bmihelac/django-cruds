4036c4d6075dec0f104334085a17f09091070c5a
Bojan Mihelac <bmihelac@mihelac.org>
2014-06-09 15:01:46 +0200

    Handle and FK returns link to detail view if exists.

--- cruds/templatetags/crud_tags.py
+++ cruds/templatetags/crud_tags.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Model
 from django import template
 from django.core.urlresolvers import (
     NoReverseMatch,
     reverse,
 )
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 from cruds import utils
+
 
 register = template.Library()
 
@@ -29,3 +33,18 @@ def crud_url(obj, action):
     except NoReverseMatch:
         url = None
     return url
+
+
+@register.filter
+def format_value(obj, field_name):
+    """
+    Simple value formatting.
+
+    If value is model instance returns link to detail view if exists.
+    """
+    value = getattr(obj, field_name)
+    if isinstance(value, Model):
+        url = crud_url(value, utils.ACTION_DETAIL)
+        if url:
+            return mark_safe('<a href="%s">%s</a>' % (url, escape(value)))
+    return value
