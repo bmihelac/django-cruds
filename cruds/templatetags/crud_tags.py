934a2fe8be3b471328ca5a26102883ec6ed8bba1
Bojan Mihelac <bmihelac@mihelac.org>
2014-06-09 14:39:42 +0200

    Do not use get_absolute_url for detail view

--- cruds/templatetags/crud_tags.py
+++ cruds/templatetags/crud_tags.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django import template
+from django.core.urlresolvers import (
+    NoReverseMatch,
+    reverse,
+)
 
+from cruds import utils
 
 register = template.Library()
 
@@ -13,3 +18,14 @@ def get_attr(obj, attr):
     Filter returns obj attribute.
     """
     return getattr(obj, attr)
+
+
+@register.assignment_tag
+def crud_url(obj, action):
+    try:
+        url = reverse(
+            utils.crud_url_name(type(obj), action),
+            kwargs={'pk': obj.pk})
+    except NoReverseMatch:
+        url = None
+    return url
