8e00934eb439515bc4fbace59c4b4497a705fb24
Bojan Mihelac <bmihelac@mihelac.org>
2024-06-24 18:19:13 +0200

    feat: remove nose test dependency

--- runtests.py
+++ runtests.py
@@ -1,57 +1,54 @@
 import sys
 
-try:
-    from django.conf import settings
+from django.conf import settings
+from django.test.runner import DiscoverRunner
 
-    settings.configure(
-        DEBUG=True,
-        USE_TZ=True,
-        DATABASES={
-            "default": {
-                "ENGINE": "django.db.backends.sqlite3",
-            }
+
+settings.configure(
+    DEBUG=True,
+    USE_TZ=True,
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+        }
+    },
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sites",
+        "cruds",
+        "tests.testapp",
+    ],
+    SITE_ID=1,
+    ROOT_URLCONF="tests.testapp.urls",
+    TEMPLATES=[
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [],
+            "APP_DIRS": True,
+            "OPTIONS": {},
         },
-        INSTALLED_APPS=[
-            "django.contrib.admin",
-            "django.contrib.auth",
-            "django.contrib.contenttypes",
-            "django.contrib.sites",
-            "cruds",
-            "tests.testapp",
-        ],
-        SITE_ID=1,
-        NOSE_ARGS=['-s'],
-        ROOT_URLCONF='tests.testapp.urls',
-        TEMPLATES=[
-            {
-                'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [],
-                'APP_DIRS': True,
-                'OPTIONS': {},
-            },
-        ],
-        SECRET_KEY="secret-key"
-    )
+    ],
+    SECRET_KEY="secret-key",
+)
 
-    try:
-        import django
-        setup = django.setup
-    except AttributeError:
-        pass
-    else:
-        setup()
+try:
+    import django
 
-    from django_nose import NoseTestSuiteRunner
-except ImportError:
-    raise ImportError("To fix this error, run: pip install -r requirements-test.txt")
+    setup = django.setup
+except AttributeError:
+    pass
+else:
+    setup()
 
 
 def run_tests(*test_args):
     if not test_args:
-        test_args = ['tests']
+        test_args = ["tests"]
 
     # Run tests
-    test_runner = NoseTestSuiteRunner(verbosity=1)
+    test_runner = DiscoverRunner(verbosity=1)
 
     failures = test_runner.run_tests(test_args)
 
@@ -59,5 +56,5 @@ def run_tests(*test_args):
         sys.exit(failures)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_tests(*sys.argv[1:])
